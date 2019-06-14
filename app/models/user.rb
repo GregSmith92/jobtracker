be86a2cc61ce4f0e9b8240360593099113130186
Greg Smith <greg_smith1992@hotmail.com>
2019-06-14 16:23:45 +0100

    Removed validation line in the user model that was causing a bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :jobs, dependent: :destroy
-  validates :name, presence: true
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # controllers: { registrations: 'users/registrations'}
   # root to: 'pages#home'
   root to: 'jobs#index'
   resources :jobs
