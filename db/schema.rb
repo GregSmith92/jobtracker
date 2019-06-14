56796b0290ca84f5575f27aaefcbaa5fc19740bf
Greg Smith <greg_smith1992@hotmail.com>
2019-06-14 14:30:20 +0100

    Added new column 'progress' to jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_120958) do
+ActiveRecord::Schema.define(version: 2019_06_14_132816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_120958) do
     t.string "link_to_job"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "progress"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
